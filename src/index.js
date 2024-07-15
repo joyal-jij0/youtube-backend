fac4260dd45d77daf667d11752113a4a10ec014d
joyal-jij0 <joyal2405@gmail.com>
2024-07-15 23:09:34 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import 'dotenv/config'
+
+import connectDB from "./db/index.js"
+
+
+
+connectDB()
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGOOSE_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("ERROR", error)
+        throw error
+    }
+})()
+
+*/
